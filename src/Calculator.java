0195e8548c289b9bf972102abe0267f94485ac04
yunus <yunussahinkaya@gmail.com>
2019-01-19 13:19:54 -0500

    write add method

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,16 @@
 public class Calculator {
 
-     public static double add(double num1, double num2){
-         return num1 + num2;
-     }
+    public static double add(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public static double subtract(double num1, double num2){
+        double  result = num1 - num2;
+        return result;
+    }
+
+    public static double multiply(double num1, double num2){
+
+        return num1*num2;
+    }
 }
